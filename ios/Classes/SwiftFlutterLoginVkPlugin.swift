c816fe4b73dfea25f97252859f999b545d9232cf
greymag <greymag@gmail.com>
2020-06-09 14:20:05 +0300

    [ios] Method, args, handle.

--- ios/Classes/SwiftFlutterLoginVkPlugin.swift
+++ ios/Classes/SwiftFlutterLoginVkPlugin.swift
@@ -1,6 +1,21 @@
 import Flutter
 import UIKit
 
+/// Plugin methods.
+enum PluginMethod: String {
+    case initSdk, logIn, logOut, getAccessToken, getUserProfile, getSdkVersion
+}
+
+/// Arguments for method `PluginMethod.initSdk`
+enum InitSdkArg: String {
+    case appId
+}
+
+/// Arguments for method `PluginMethod.logIn`
+enum LogInArg: String {
+    case scope
+}
+
 public class SwiftFlutterLoginVkPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_login_vk", binaryMessenger: registrar.messenger())
@@ -9,6 +24,65 @@ public class SwiftFlutterLoginVkPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+        guard let method = PluginMethod(rawValue: call.method) else {
+            result(FlutterMethodNotImplemented)
+            return
+        }
+        
+        switch method {
+        case .initSdk:
+            guard
+                let args = call.arguments as? [String: Any],
+                let appIdArg = args[InitSdkArg.appId.rawValue] as? String
+                else {
+                    result(FlutterError(code: "INVALID_ARGS",
+                                        message: "Arguments is invalid",
+                                        details: nil))
+                    return
+            }
+            
+            initSdk(result: result, appId: appIdArg)
+        case .logIn:
+            guard
+                let args = call.arguments as? [String: Any],
+                let permissionsArg = args[LogInArg.scope.rawValue] as? [String]
+                else {
+                    result(FlutterError(code: "INVALID_ARGS",
+                                        message: "Arguments is invalid",
+                                        details: nil))
+                    return
+            }
+            
+            logIn(result: result, permissions: permissionsArg)
+        case .logOut:
+            logOut(result: result)
+        case .getAccessToken:
+            getAccessToken(result: result)
+        case .getUserProfile:
+            getUserProfile(result: result)
+        case .getSdkVersion:
+            getSdkVersion(result: result)
+        }
   }
+    
+    // Plugin methods impl
+    
+    private func initSdk(result: @escaping FlutterResult, appId: String) {
+    
+    }
+    
+    private func logIn(result: @escaping FlutterResult, permissions: [String]) {
+    }
+    
+    private func logOut(result: @escaping FlutterResult) {
+    }
+    
+    private func getAccessToken(result: @escaping FlutterResult) {
+    }
+    
+    private func getUserProfile(result: @escaping FlutterResult) {
 }
+
+    private func getSdkVersion(result: @escaping FlutterResult) {
+        result(VK_SDK_VERSION)
+    }
